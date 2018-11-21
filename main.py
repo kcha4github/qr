85682fd0fddf3abe176dbee718eb66cf507644c2
kcha4github <kunio.tsuge@gmail.com>
2018-11-21 19:56:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ def stream():
 def gen():
   while True:
     frame = get_frame()
-    yield(b'--frame¥r¥n'
-          b'Content-Type: image/jpeg¥r¥n¥r¥n' + frame + b'¥r¥n¥r¥n')
+    yield(b'--frame\r\n'
+          b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 def get_frame():
   camera.capture(rawCapture, format="bgr", use_video_port=True)
